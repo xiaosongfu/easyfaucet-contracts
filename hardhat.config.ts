55ccd7151b4e8bd933cb0a51f1fc9d9a649d94aa
xiaosongfu <xiaosong.fu@outlook.com>
2025-11-09 22:55:32 +0800

    deploy to bsc testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,16 +13,13 @@ const config: HardhatUserConfig = {
     profiles: {
       default: {
         version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
         settings: {
           optimizer: {
             enabled: true,
             runs: 200,
           },
         },
-      },
+      }
     },
   },
   networks: {
